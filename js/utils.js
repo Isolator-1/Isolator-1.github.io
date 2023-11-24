f660d0ae7dc80eae35335a031b1383d39fa90405
Isolator <55656501+Isolator-1@users.noreply.github.com>
2023-11-24 21:00:47 +0800

    Site updated: 2023-11-24 21:00:42

--- js/utils.js
+++ js/utils.js
@@ -4,16 +4,17 @@ window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequ
 
 
 
-var targetElement = document.getElementById('board'); 
-pseudoElement.style.backgroundImage = "url('/img/chtholly4.jpg')";
-pseudoElement.style.opacity = 0.5;
-pseudoElement.style.position = 'absolute';
-pseudoElement.style.top = 0;
-pseudoElement.style.left = 0;
-pseudoElement.style.width = '100%';
-pseudoElement.style.height = '100%';
-pseudoElement.style.zIndex = -1;
-targetElement.appendChild(pseudoElement);
+// var targetElement = document.getElementById('board'); 
+// var pseudoElement = document.createElement('div');
+// pseudoElement.style.backgroundImage = "url('/img/chtholly4.jpg')";
+// pseudoElement.style.opacity = 0.5;
+// pseudoElement.style.position = 'absolute';
+// pseudoElement.style.top = 0;
+// pseudoElement.style.left = 0;
+// pseudoElement.style.width = '100%';
+// pseudoElement.style.height = '100%';
+// pseudoElement.style.zIndex = -1;
+// targetElement.appendChild(pseudoElement);
 
 
 Fluid.utils = {
