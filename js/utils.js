c5597589f8fc14965db8fa3fc5fa78b8d3af0874
Isolator <55656501+Isolator-1@users.noreply.github.com>
2023-11-24 21:18:11 +0800

    Site updated: 2023-11-24 21:18:00

--- js/utils.js
+++ js/utils.js
@@ -3,20 +3,6 @@
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
 
 
-
-// var targetElement = document.getElementById('board'); 
-// var pseudoElement = document.createElement('div');
-// pseudoElement.style.backgroundImage = "url('/img/chtholly4.jpg')";
-// pseudoElement.style.opacity = 0.5;
-// pseudoElement.style.position = 'absolute';
-// pseudoElement.style.top = 0;
-// pseudoElement.style.left = 0;
-// pseudoElement.style.width = '100%';
-// pseudoElement.style.height = '100%';
-// pseudoElement.style.zIndex = -1;
-// targetElement.appendChild(pseudoElement);
-
-
 Fluid.utils = {
 
   listenScroll: function(callback) {
